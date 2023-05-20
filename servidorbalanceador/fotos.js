3a7d9f53915bece458cec091db5d940a2a06667d
danielhc144 <danielhc144@gmail.com>
2023-05-20 04:20:42 +0000

    Proyecto T3

---
+++ servidorbalanceador/fotos.js
@@ -0,0 +1,21 @@
+const API_URL = 'https://jsonplaceholder.typicode.com';
+
+fetch(`${API_URL}/photos`)
+  .then(response => response.json())
+  .then(photos => {
+    const appDiv = document.getElementById('app');
+    const photoList = document.createElement('ul');
+    
+    photos.forEach(photo => {
+      const photoItem = document.createElement('li');
+      const photoImg = document.createElement('img');
+      photoImg.src = photo.thumbnailUrl;
+      photoImg.alt = photo.title;
+      photoItem.appendChild(photoImg);
+      photoList.appendChild(photoItem);
+    });
+    
+    appDiv.appendChild(photoList);
+  })
+  .catch(error => console.error(error));
+
